766498441dd71be98e984accd5cd6be8eb759264
Oli Dacombe <olidacombe@gmail.com>
2017-10-17 00:25:48 +0100

    making room for some debug output

--- src/Ps3Controller.cpp
+++ src/Ps3Controller.cpp
@@ -4,13 +4,9 @@
 
 Ps3Controller::Ps3Controller()
 {
-    struct hid_device_info *devs, *dev_i;;
+    inputBuffer.fill(0);
 
     device = hid_open(0x54c, 0x0268, NULL); // device = NULL on failure
-
-#ifdef discern_arse_elbow
-    getFeatureReport();
-#endif
 
     readData();
 }
@@ -19,37 +15,27 @@ bool Ps3Controller::readData()
 {
   if(device==nullptr) return false;
 
-  const int readResult = hid_read(device, inputBuffer, inputBufferSize);
+  const int readResult = hid_read(device, inputBuffer.data(), inputBufferSize.size());
   if(readResult==-1) return false; // error, may have lost device and need to slow scan
   if(readResult==0) { /* we're non-blocking and there's no data available at the mo */ }
 
-  // debug dump
   else {
+    // debug dump
+    /*
     for(int i=0; i<readResult; i++) {
       printf("%02hx ", inputBuffer[i]);
     }
     printf("\n");
+    */
   }
 
   return true;
 }
 
-#ifdef discern_arse_elbow
-void Ps3Controller::getFeatureReport()
+const auto Ps3Controller::getData()
 {
-  if(device==nullptr) return;
-
-  inputBuffer[0] = 0x0;
-  const int report0n = hid_get_feature_report(device, inputBuffer, inputBufferSize);
-  printf("report0n: %i\n", report0n);
-  inputBuffer[0] = 0x1;
-  const int report1n = hid_get_feature_report(device, inputBuffer, inputBufferSize);
-  printf("report1n: %i\n", report1n);
-  inputBuffer[0] = 0x2;
-  const int report2n = hid_get_feature_report(device, inputBuffer, inputBufferSize);
-  printf("report2n: %i\n", report2n);
+    return inputBuffer;
 }
-#endif
 
 Ps3Controller::~Ps3Controller()
 {
--- src/Ps3Controller.h
+++ src/Ps3Controller.h
@@ -7,17 +7,16 @@
 class Ps3Controller
 {
     hid_device *device;
-    static const size_t inputBufferSize = 1024;
+    //static const size_t inputBufferSize = 1024;
     //unsigned char[50] buf; // received 49 bytes in test, +1 for report num
-    unsigned char inputBuffer[inputBufferSize]; // let's see how much data we can get before trimming
-
-#ifdef discern_arse_elbow
-    void getFeatureReport();
-#endif
+    //unsigned char inputBuffer[inputBufferSize]; // let's see how much data we can get before trimming
+    std::array<unsigned char, 1024> inputBuffer;
 
     bool readData();
 
 public:
     Ps3Controller();
     ~Ps3Controller();
+
+    const std::array& getData();
 };
